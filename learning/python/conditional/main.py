e69527d6827ca25eb089c2cf064bfa84416efdee
alifathi24 <fathialo81@gmail.com>
2024-07-30 08:57:03 -0600

    update

---
+++ learning/python/conditional/main.py
@@ -0,0 +1,19 @@
+# if True:
+#   print('Conditional was True')
+
+# if False:
+#   print('Conditional was True')
+
+# language = 'Python'
+# if language == 'Python':
+#   print('Conditional was True')
+
+user = 'Admin'
+logged_in = True
+
+if user == 'Admin' and logged_in:
+  print('Admin Page')
+else:
+  print('Bad Creds')
+
+
