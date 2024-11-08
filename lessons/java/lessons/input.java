4d4cc1bb216f4e9834a2844bedaaeaebb7a7417b
Fathi Ali <fathiali@Fathis-MacBook-Pro.local>
2024-11-08 16:36:05 -0700

    update project folder

---
+++ lessons/java/lessons/input.java
@@ -0,0 +1,11 @@
+package lessons;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String inputs = scanner.nextLine();
+        // System.out.println(inputs.indexO));
+    }
+}
