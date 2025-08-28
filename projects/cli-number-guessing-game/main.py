7f11cc851ae895022af18258315e0f949869c0a4
alifathi24 <fathialo81@gmail.com>
2025-08-28 16:17:43 -0600

    first solution

---
+++ projects/cli-number-guessing-game/main.py
@@ -0,0 +1,17 @@
+# TODO: import random module
+import random
+
+#TODO: Ask the user for number to guess
+num = int(input("Enter a number: "))
+#TODO: Generator random 
+random_choice = random.randint(0, 101)
+print(random_choice)
+
+if random_choice > num:
+    print("Please try again!")
+elif random_choice < num:
+    print("Please try again!")
+elif random_choice == num:
+    print("You got it!")
+else:
+    print("Please try again!")
