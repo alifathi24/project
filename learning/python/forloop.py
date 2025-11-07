07d66dcd49f5ff66a3d518ce7fe15fe6216572e9
alifathi24 <fathialo81@gmail.com>
2025-11-06 22:18:51 -0700

    update

--- learning/python/forloop.py
+++ learning/python/forloop.py
@@ -2,3 +2,19 @@
 
 # for fruit in fruits:
 #     print(fruit)
+#     print(fruit + " pie!")
+
+# student_score = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, ]
+
+# max_score = 0
+
+# for score in student_score:
+#     if score > max_score:
+#         max_score = score
+
+# print(max_score)
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
---
+++ projects/ios/SayHello/SayHello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayHello
+//
+//  Created by Fathi Ali on 2025-09-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
