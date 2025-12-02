c561652543ac042ea1a68037240edb110a2e0075
alifathi24 <fathialo81@gmail.com>
2025-12-02 05:11:58 -0700

    make folder for projects in learning folder

--- learning/IOS/Apps/ColorSwitcher/ColorSwitcher/ViewController.swift
+++ learning/IOS/Apps/ColorSwitcher/ColorSwitcher/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl1: UILabel!
+    
+    @IBOutlet weak var lbl2: UILabel!
+    @IBOutlet weak var lbl3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = .darkGray
+        
+        lbl1.textColor = .white
+        lbl1.backgroundColor = .green
+        lbl1.text = "Label 1"
+        
+        lbl2.textColor = .white
+        lbl2.backgroundColor = .blue
+        lbl2.text = "Label 2"
+        
+        lbl3.textColor = .white
+        lbl3.backgroundColor = .red
+        lbl3.text = "Label 3"
+        
+        
+        
+        
+        
+        
+    }
+    
 }
 
---
+++ learning/IOS/Apps/I AM Rich/I AM Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by Fathi Ali on 2025-11-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
