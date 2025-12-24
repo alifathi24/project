fa1c2247842a9fdfb729542cd8910e9c5581cfaf
alifathi24 <fathialo81@gmail.com>
2025-12-24 12:39:39 -0700

    calculator project

--- projects/java/calculator/calculator.java
+++ projects/java/calculator/calculator.java
@@ -1,20 +1,47 @@
 package calculator;
 import java.util.*;
 
-public class calculator {
+public class Calculator {
    public static void main(String arg[]){
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome to CLI calculator: ");
-    System.out.println("Please enter the first number: ");
-    int firstDigit = scan.nextInt();
-   System.out.println("Please enter the second number: ");
-    int secondDigit = scan.nextInt();
-    System.out.println("Enter an operation, +, -, /, *");
-    String operation = scan.next();
+      Scanner scan = new Scanner(System.in);
+      // String addition = "+";
+      // String substraction = "-";
+      // String mutliplication = "*";
+      // String divition = "/";
+     
+      System.out.println("Welcome to CLI calculator: ");
+      System.out.println("Please enter the first number: ");
+    double firstDigit = scan.nextDouble();
+      System.out.println("Please enter the second number: ");
+      
+    double secondDigit = scan.nextDouble();
+      System.out.println("Enter an operation, +, -, /, *");
+      String operation = scan.next();
 
-    System.out.println("Operation is " + operation);
-    System.out.println(firstDigit);
-    System.out.println(secondDigit);
-    
+      // System.out.println("Operation is " + operation);
+      // System.out.println(firstDigit);
+      // System.out.println(secondDigit);
+      if (secondDigit == 0) {
+         System.out.println("Erro division by zero.");
+      }
+
+      else  if (operation.equals("+")) {
+         System.out.println(firstDigit + " + " + secondDigit + " = " + (firstDigit + secondDigit));
+    }
+      else if (operation.equals("-")) {
+         System.out.println(firstDigit + " - " + secondDigit + " = " + (firstDigit - secondDigit));
+      }
+    else if (operation.equals("*")) {
+         System.out.println(firstDigit + " * " + secondDigit + " = " + (firstDigit * secondDigit));
+    }
+    else if (operation.equals("/")) {
+         System.out.println(firstDigit + " / " + secondDigit + " = " + (firstDigit / secondDigit));
+    }
+
+    else {
+      System.out.println("Invalid operation");
+    }
+
+    scan.close();
    } 
 }
