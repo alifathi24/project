6eb062e9d5c1462c6e9a673c7f6176e86e050633
alifathi24 <fathialo81@gmail.com>
2026-01-12 01:39:40 -0700

    update

--- projects/java/expense-tracker.com/src/main/Main.java
+++ projects/java/expense-tracker.com/src/main/Main.java
@@ -7,10 +7,12 @@ public class Main {
         LocalDate today = LocalDate.now();
 
         System.out.println("Today's date is: " + today);
-        System.out.println("Enter the expense category:\n\n\t 1.Grocery\n\t 2.Rent\n\t 3.Utilities\n\t");
+        System.out.println("Enter the expense category:\n\n\t 1.Grocery\n\t 2.Rent\n\t 3.Utilities\n\t 4.Internet and Substription\n\t 5.Phone\n\t 6.Transportation\n\t 7.Other\n");
 
+        int expenseCategory = Integer.valueOf(scan.nextInt());
         double expenseAmount = Double.valueOf(scan.nextDouble());
-
+        System.out.println("Category is: " + expenseCategory);
+        System.out.println("Expense amount is $" + expenseAmount);
         
 
 
