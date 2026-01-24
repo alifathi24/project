93053e106e5ef0b4e0c3d196b0a646e21dcb859e
alifathi24 <fathialo81@gmail.com>
2026-01-24 02:51:37 -0700

    add simple switch and menu

--- projects/java/expense-tracker.com/src/main/Main.java
+++ projects/java/expense-tracker.com/src/main/Main.java
@@ -5,15 +5,27 @@ public class Main {
     public static void main (String args[]){
         Scanner scan = new Scanner(System.in);
         LocalDate today = LocalDate.now();
-
-        System.out.println("Today's date is: " + today);
-        System.out.println("Enter the expense category:\n\n\t 1.Grocery\n\t 2.Rent\n\t 3.Utilities\n\t 4.Internet and Substription\n\t 5.Phone\n\t 6.Transportation\n\t 7.Other\n");
+        System.out.println("Welcome to CLI expense tracker");
+        System.out.println("======================================");
+        System.out.println("\n\tToday's date is: " + today);
+        System.out.println("======================================");
+        System.out.println("\tEnter the expense category:\n\n\t 1.Grocery\n\t 2.Rent\n\t 3.Utilities\n\t 4.Internet and Substription\n\t 5.Phone\n\t 6.Transportation\n\t 7.Other\n");
 
         int expenseCategory = Integer.valueOf(scan.nextInt());
         double expenseAmount = Double.valueOf(scan.nextDouble());
         System.out.println("Category is: " + expenseCategory);
         System.out.println("Expense amount is $" + expenseAmount);
         
+        switch (expenseCategory) {
+            case 1:
+                System.out.println("Expense is: " + expenseCategory);
+                System.out.println("Its cost is: $" + expenseAmount);
+                
+                break;
+        
+            default:
+                break;
+        }
 
 
         
